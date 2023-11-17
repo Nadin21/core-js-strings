a38a5638ffdc12a710c0ead1e5a90bf99f92d7bd
Nadin21 <nadin21dir@gmail.com>
2023-11-17 11:05:56 +0300

    feat: sumOfCodes, startsWith, endsWith

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -185,8 +185,12 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (!str) return 0;
+  return str
+    .split('')
+    .map((item) => item.charCodeAt())
+    .reduce((sum, char) => sum + Number(char), 0);
 }
 
 /**
@@ -200,8 +204,8 @@ function sumOfCodes(/* str */) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -215,8 +219,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
